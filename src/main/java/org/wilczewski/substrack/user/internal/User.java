582b633e5ede831a96a2bbfed96d6f011f191cbc
Wiktor Wilczewski <82906404+Czewski04@users.noreply.github.com>
2026-07-21 23:26:01 +0200

    feat(user): enhance User entity with additional email management methods

--- src/main/java/org/wilczewski/substrack/user/internal/User.java
+++ src/main/java/org/wilczewski/substrack/user/internal/User.java
@@ -3,6 +3,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -15,18 +16,19 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(name = "username", nullable = false)
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
     @Column(name = "email",  nullable = false, unique = true)
     private String email;
 
-    @Column(name = "password_hash", nullable = false,  unique = true)
+    @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -40,11 +42,31 @@ class User {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @Builder
-    public User(String username, String email, String passwordHash) {
-        this.username = username;
-        this.email = email;
-        this.passwordHash = passwordHash;
-        this.additionalEmails.add(new UserEmail(email, this));
+    public void addAdditionalEmail(String email) {
+        UserEmail newEmail = new UserEmail(email, this);
+        this.additionalEmails.add(newEmail);
+    }
+
+    public void updateAdditionalEmail(UUID emailId, String newEmail) {
+        for (UserEmail userEmail : additionalEmails) {
+            if (userEmail.getId().equals(emailId)) {
+                if(userEmail.getEmail().equals(this.email)) {
+                    this.email = newEmail;
+                }
+                userEmail.setEmail(newEmail);
+                return;
+            }
+        }
+    }
+
+    public void  deleteAdditionalEmail(UUID emailId) throws Exception {
+        for (UserEmail userEmail : additionalEmails) {
+            if (userEmail.getId().equals(emailId)) {
+                if(userEmail.getEmail().equals(this.email)) {
+                    throw new Exception("Cannot delete primary email");
+                }
+                additionalEmails.remove(userEmail);
+            }
+        }
     }
 }
