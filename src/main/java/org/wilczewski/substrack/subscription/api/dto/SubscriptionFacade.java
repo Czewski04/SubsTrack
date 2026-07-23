f9ce09646e889d27c5de622874997634757cbe9e
Wiktor Wilczewski <82906404+Czewski04@users.noreply.github.com>
2026-07-23 02:37:29 +0200

    feat(subscription): add method to get all active subscriptions

--- src/main/java/org/wilczewski/substrack/subscription/api/dto/SubscriptionFacade.java
+++ src/main/java/org/wilczewski/substrack/subscription/api/dto/SubscriptionFacade.java
@@ -3,10 +3,12 @@ package org.wilczewski.substrack.subscription.api.dto;
 import org.wilczewski.substrack.subscription.api.dto.query.GetUserSubscriptionQuery;
 import org.wilczewski.substrack.subscription.api.dto.response.SubscriptionResponse;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SubscriptionFacade {
     boolean subscriptionExist(UUID subscriptionId);
     SubscriptionResponse getSubscriptionById(GetUserSubscriptionQuery query);
     void validateSubscription(UUID userId, UUID subscriptionId);
+    List<SubscriptionResponse> getAllActiveSubscriptions();
 }
--- src/main/java/org/wilczewski/substrack/subscription/internal/SubscriptionRepository.java
+++ src/main/java/org/wilczewski/substrack/subscription/internal/SubscriptionRepository.java
@@ -1,7 +1,7 @@
 package org.wilczewski.substrack.subscription.internal;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +9,7 @@ import java.util.UUID;
 
 @Repository
 interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
-    Boolean existsByName(String name);
-    Subscription findByName(String name);
     List<Subscription> findAllByUserId(UUID userId);
-    List<Subscription> findAllByEmailId(UUID emailId);
+    @Query("select s from Subscription s where s.isActive = true")
+    List<Subscription> findAllByIsActiveTrue();
 }
