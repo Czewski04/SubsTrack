e8e98ee72ecb7890b4808aef34a63cee0f0d9970
Wiktor Wilczewski <82906404+Czewski04@users.noreply.github.com>
2026-07-23 04:41:25 +0200

    test: add shared Testcontainers and MockMvc integration base

---
+++ src/test/java/org/wilczewski/substrack/AbstractIntegrationTest.java
@@ -0,0 +1,21 @@
+package org.wilczewski.substrack;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Import(TestcontainersConfiguration.class)
+public abstract class AbstractIntegrationTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockitoBean
+    protected JavaMailSender mailSender;
+}
